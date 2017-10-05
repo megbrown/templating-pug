8c54fe8fd503c6b2722f03e686d770a19a6b7746
Megan B <brown.m.j@outlook.com>
2017-10-05 12:02:53 -0500

    date and inventory

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('view engine', 'pug');
 
 app.get('/', (req, res, next) => {
 	res.render('index', {
-		name: "Baker",
+		name: "Bakery Bakery",
 	});
 });
 
@@ -17,9 +17,32 @@ app.get('/about', (req, res, next) => {
 	});
 });
 
+let inventory = [
+	{
+		name: 'Bagels',
+		price: '$1.00 ea.'
+	},
+	{
+		name: 'Cookies',
+		price: '$.50 ea.'
+	},
+	{
+		name: 'Muffins',
+		price: '$1.20 ea.'
+	},
+	{
+		name: 'Cupcakes',
+		price: '$1.50 ea.'
+	},
+	{
+		name: 'Donuts',
+		price: '$0.75 ea.'
+	}
+];
+
 app.get('/inventory', (req, res, next) => {
 	res.render('inventory', {
-
+		inventory: inventory
 	});
 });
 
